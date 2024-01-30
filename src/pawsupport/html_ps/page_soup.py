68412bf200c5847776f2a683a6cd1387b821e3d7
paw <paw@pawrequest.com>
2024-01-30 23:35:29 +0000

    tag soup

--- src/pawsupport/html_ps/page_soup.py
+++ src/pawsupport/html_ps/page_soup.py
@@ -1,19 +1,30 @@
 from __future__ import annotations
+
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 
 from ..async_ps import response_
 
 
-class PageSoup(BeautifulSoup):
-    """ Inject from_url classmethod into BeautifulSoup"""
+class TagSoup(BeautifulSoup):
+    """Inject from_bs4 classmethod into BeautifulSoup"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def from_bs4(cls, bs4: BeautifulSoup) -> TagSoup:
+        return cls(bs4.prettify(), "html.parser")
+
+
+class PageSoup(TagSoup):
+    """Inject from_url classmethod into BeautifulSoup"""
+
+    def __init__(self, url, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.url = url
 
     @classmethod
     async def from_url(cls, url: str, http_session: ClientSession) -> PageSoup:
         html = await response_(url, http_session)
-        return cls(html, "html.parser")
-
-    @classmethod
-    def from_bs4(cls, bs4: BeautifulSoup) -> PageSoup:
-        return cls(bs4.prettify(), "html.parser")
-
+        return cls(url, html, "html.parser")
