3943849389cd7ef3138ea7b232475795838869eb
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-02-15 23:41:53 +0000

    scripts

--- src/pawsupport/pdf_tools/array_pdf/convert_dir_and_print.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PYTHON_EXE=R:\paul_r\programs\array_pdf\.venv\Scripts\python.exe
-set SCRIPT_PATH=R:\paul_r\programs\array_pdf\array.py
-
-"%PYTHON_EXE%" "%SCRIPT_PATH%" %~dp0 --print
-
-pause
