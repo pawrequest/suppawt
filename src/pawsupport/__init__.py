76a7d3e0d7af34cf138617e493f7d24c5561a7a8
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-02-15 17:51:27 +0000

    conditional imports via can_import and imortlib

--- src/pawsupport/__init__.py
+++ src/pawsupport/__init__.py
@@ -1,6 +1,5 @@
 from .misc_ps import can_import
-from . import types_ps
-from . import convert
+from . import convert, types_ps
 
 if can_import('loguru'):
     from . import logging_ps
@@ -10,15 +9,16 @@ if can_import('loguru'):
     if can_import('aiohttp'):
         from . import async_ps
 
+        if can_import('bs4'):
+            from . import html_ps
+
     if can_import('fastui'):
         from . import fastui_ps
-
-
-if can_import('bs4'):
-    from . import html_ps
+    if can_import('context_menu'):
+        from . import context_menu_ps
 
 # internal optional = sqlmodel
 from . import backup_ps
 
-
-__all__ = ['logging_ps', 'error_ps', 'async_ps', 'misc_ps', 'types_ps', 'html_ps', 'convert', 'fastui_ps', 'backup_ps']
+__all__ = ['logging_ps', 'error_ps', 'async_ps', 'misc_ps', 'types_ps', 'html_ps', 'convert',
+           'fastui_ps', 'backup_ps', 'context_menu_ps']
