be1a32ffbda552fefb65b278883b6e989d482010
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-02-20 16:02:36 +0000

    fastui customn clases

--- src/pawsupport/fastui_ps/__init__.py
+++ src/pawsupport/fastui_ps/__init__.py
@@ -1,3 +1,13 @@
-from . import fastui_support
+from .fastui_support import (
+    ColClass,
+    DFLT_COL_CSS,
+    DFLT_CONTAINER_CSS,
+    DFLT_CSS,
+    DFLT_HEAD_CSS,
+    DFLT_PAGE_CSS,
+    DFLT_ROW_CSS,
+    RowClass,
+)
 
-__all__ = ['fastui_support']
+__all__ = ['RowClass', 'ColClass', 'DFLT_CSS', 'DFLT_COL_CSS', 'DFLT_ROW_CSS', 'DFLT_HEAD_CSS',
+           'DFLT_CONTAINER_CSS', 'DFLT_PAGE_CSS']
--- src/pawsupport/fastui_ps/fastui_support.py
+++ src/pawsupport/fastui_ps/fastui_support.py
@@ -1,164 +1,115 @@
 """
 FastUI support functions
 """
-
 from __future__ import annotations
 
-from typing import List, Protocol, Sequence
+from typing import List, Optional, Protocol
 
 from loguru import logger
-from fastui.events import BackEvent, GoToEvent
+from fastui.events import AnyEvent, BackEvent, GoToEvent
 from fastui import AnyComponent, components as c
 
-STANDARD = "text-center py-1 my-1"
-COL_4 = f"col-4 {STANDARD}"
-ROW = f'my-1 border border-bottom border-secondary rounded shadow-sm {STANDARD}'
-HEAD = f'my-1 col border {STANDARD}'
+DFLT_CSS = "text-center py-1 my-1"
+DFLT_COL_CSS = f"{DFLT_CSS}"
+DFLT_ROW_CSS = f'my-1 border border-bottom border-secondary rounded shadow-sm {DFLT_CSS}'
+DFLT_HEAD_CSS = f'my-1 col border {DFLT_CSS}'
+DFLT_CONTAINER_CSS = f'{DFLT_CSS}'
+DFLT_PAGE_CSS = f'{DFLT_CSS}'
 
 
-class FastUiMaker:
-    ...
+class RowClass(DivPR):
 
-
-def cols_from_list(components: List[AnyComponent], class_name: str = '') -> List[c.Div]:
-    return [Col(components=[comp], col_class=class_name) for comp in components]
-
-
-def rows_from_list(components: List[AnyComponent], class_name: str = '') -> List[c.Div]:
-    return [Row(components=[comp], row_class_name=class_name) for comp in components]
-
-
-def Row(
-        components: List[AnyComponent],
-        row_class_name: str = ROW,
-        sub_cols=False,
-        col_class_name=''
-) -> c.Div:
-    """
-    Create a row of fastui components
-
-    :param components: a list of fastui components
-    :param row_class_name: css class names as a string
-    :param sub_cols: if True, create sub columns
-    :param col_class_name: css class names for sub columns as a string
-
-    :returns: Div with class_name='row'+class_name
-    """
-    try:
-        # return c.Div(components=components, class_name="row")
-        if not components:
-            return c.Div(components=[c.Text(text="---")])
+    def __init__(
+            self,
+            components: List[AnyComponent],
+            row_class_name: str = DFLT_ROW_CSS,
+            sub_cols=False,
+            col_class_name=''
+    ):
         row_class_name = f"row {row_class_name}"
-        if sub_cols:
-            components = cols_from_list(components, class_name=col_class_name)
-        return c.Div(components=components, class_name=row_class_name)
-    except Exception as e:
-        logger.error(e)
-
-
-def Col(
-        components: List[AnyComponent],
-        col_class: str = '',
-        sub_rows: bool = False,
-        row_class: str = ''
-) -> c.Div:
-    """
-    Create a column of fastui components
-
-    args:
-        components: a list of fastui components
-        col_class: css class names as a string
-        sub_rows: if True, create sub rows
-        row_class: css class names for sub rows as a string
-
-    returns: Div with class_name='col'+class_name
-    """
-
-    try:
-        col_class = f"col {col_class}"
-        if sub_rows:
-            components = rows_from_list(components, class_name=row_class)
-        return c.Div(components=components, class_name=col_class)
-    except Exception as e:
-        logger.error(e)
-
-
-def empty_div(col=False, container=False) -> c.Div:
-    """
-    Create an empty div
-
-    :param col: if True, create an empty col
-    :param container: if True, create an empty container
-
-    :returns: Div with text '---'
-    """
-    if col:
-        return empty_col()
-    elif container:
-        return empty_container()
-    else:
-        return c.Div(components=[c.Text(text="---")])
-
-
-def empty_col():
-    """
-    Create an empty col
-
-    :returns: Div with text '---' and class_name='col'
-    """
-    return Col(components=[c.Text(text="---")])
-
-
-def empty_container():
-    """
-    Create an empty container
-
-    :returns: Div with text '---' and class_name='container'
-    """
-    return Flex(components=[c.Text(text="---")])
-
-
-def Flex(components: list[AnyComponent], class_name="") -> c.Div:
-    """
-    Create a flex container
-
-    :param components: a list of fastui components
-    :param class_name: css class names as a string
-    :returns: Div with class_name='d-flex'+class_name
-    """
-
-    logger.info("Flex")
-    try:
         if not components:
-            return c.Div(components=[c.Text(text="---")])
-    except Exception as e:
-        logger.error(e)
-    try:
-        # class_name = f"container border-bottom border-secondary {class_name}"
-        # class_name = f"d-flex border-bottom border-secondary {class_name}"
-        return c.Div(components=components, class_name=class_name)
-    except Exception as ee:
-        logger.error(ee)
+            components = [DivPR.empty()]
+        if sub_cols:
+            components = ColClass.from_list_many(components, class_name=col_class_name)
+        super().__init__(components=components, class_name=row_class_name)
+
+    @classmethod
+    def from_list_many(cls, components: List[AnyComponent], class_name: str = '') -> List[DivPR]:
+        return [cls(components=[comp], row_class_name=class_name) for comp in components]
+
+    @classmethod
+    def from_list(cls, components: List[AnyComponent], class_name: str = '') -> DivPR:
+        return cls(components=components, row_class_name=class_name)
 
 
-def default_page(
-        components: list[AnyComponent], title: str, navbar, header_class=None,
-        page_classname=None
-) -> list[AnyComponent]:
-    """
-    Create a default page
+class ColClass(DivPR):
 
-    :param components: a list of fastui components
-    :param title: page title
-    :param navbar: navbar
-    :param header_class: css class names for header as a string
-    :param page_classname: css class names for page as a string
-    :returns: list of fastui components
-    """
-    try:
+    def __init__(
+            self,
+            components: List[AnyComponent],
+            col_class: str = DFLT_COL_CSS,
+            sub_rows: bool = False,
+            row_class: str = DFLT_ROW_CSS
+    ):
+        col_class = f"col {col_class}"
+        if not components:
+            components = [DivPR.empty()]
+        if sub_rows:
+            components = RowClass.from_list_many(components, class_name=row_class)
+        super().__init__(components=components, class_name=col_class)
+
+    @classmethod
+    def from_list_many(cls, components: List[AnyComponent], class_name: str = DFLT_COL_CSS) -> List[
+        ColClass]:
+        return [cls(components=[comp], col_class=class_name) for comp in components]
+
+    @classmethod
+    def from_list(cls, components: List[AnyComponent], class_name: str = DFLT_COL_CSS) -> DivPR:
+        return cls(components=components, col_class=class_name)
+
+    @classmethod
+    def empty(cls):
+        return cls(components=[DivPR.empty()])
+
+
+class DivPR(DivPR):
+    @classmethod
+    def empty(cls):
+        return cls(components=[c.Text(text="---")])
+
+
+class Container(DivPR):
+    def __init__(
+            self,
+            components: List[AnyComponent],
+            class_name: str = DFLT_CONTAINER_CSS
+    ) -> None:
+        class_name = f"container {class_name}"
+        super().__init__(components=components, class_name=class_name)
+
+    @classmethod
+    def from_list(cls, components: List[AnyComponent], class_name: str = '') -> DivPR:
+        return cls(components=components, class_name=class_name)
+
+    @classmethod
+    def empty(cls):
+        return cls(components=[DivPR.empty()])
+
+
+class PagePR(c.Page):
+    @classmethod
+    def default_page(
+            cls,
+            components: list[AnyComponent],
+            title: str = '',
+            navbar=None,
+            footer=None,
+            header_class=DFLT_HEAD_CSS,
+            page_classname=DFLT_PAGE_CSS
+    ) -> list[AnyComponent]:
         return [
-            c.PageTitle(text=title),
-            navbar,
+            c.PageTitle(text=f'PawRequest dev - {title}' if title else ''),
+            *((navbar,) if navbar else ()),
             c.Page(
                 components=[
                     *((c.Heading(text=title, class_name=header_class),) if title else ()),
@@ -166,101 +117,53 @@ def default_page(
                 ],
                 class_name=page_classname,
             ),
-            default_footer(),
+            *((footer,) if footer else ()),
         ]
-    except Exception as e:
-        logger.error(e)
 
-
-def empty_page(nav_bar=None) -> list[AnyComponent]:
-    """
-    Create an empty page
-
-    :param nav_bar: navbar
-    :returns: list of fastui components
-    """
-    return [
-        c.PageTitle(text="empty page"),
-        (nav_bar if nav_bar else empty_div()),
-        c.Page(
+    @classmethod
+    def empty_page(cls, nav_bar=None) -> list[AnyComponent]:
+        return cls.default_page(
+            title="empty page",
             components=[
-                c.Heading(text="durfault title"),
-                c.Text(text="---"),
+                c.Heading(text="empty page"),
+                DivPR.empty(),
             ],
-        ),
-        default_footer(),
-    ]
+        )
 
 
-def default_footer() -> c.Footer:
-    """
-    Create a default footer
+class LinkPR(c.Link):
+    @classmethod
+    def custom(cls, title: str, url: str, on_click: Optional[AnyEvent] = None, class_name=""):
+        if not url and not on_click:
+            logger.error("Must provide url or on_click event")
 
-    :returns: Footer with extra_text='extra durfault text' and a link to github
-    """
-    return c.Footer(
-        extra_text="extra durfault text",
-        links=[
-            c.Link(
-                components=[c.Text(text="Github")],
-                on_click=GoToEvent(url="https://github.com/pydantic/FastUI"),
-            ),
-        ],
-    )
+        on_click = on_click or GoToEvent(url=url)
 
+        return cls(components=[c.Text(text=title)], on_click=on_click, class_name=class_name)
 
-def back_link() -> c.Link:
-    """
-    Create a back link
+    @classmethod
+    def back(cls) -> c.Link:
+        return cls(
+            title="Back",
+            url="",
+            on_click=BackEvent(),
+        )
 
-    :returns: Link with text='Back' and on_click=BackEvent()
-    """
-    return c.Link(components=[c.Text(text="Back")], on_click=BackEvent())
-
-
-def nav_bar_from_routable(models: Sequence[RoutableModel]) -> c.Navbar:
-    """
-    Create a navbar
-
-    :param models: a list of RoutableModel
-    :returns: Navbar with links to each RoutableModel
-    """
-    return c.Navbar(
-        links=[nav_link(_) for _ in models],
-    )
+    @classmethod
+    def model_nav(cls, model: RoutableModel) -> c.Link:
+        return cls(
+            components=[c.Text(text=model.__name__.title())],
+            on_click=GoToEvent(url=model.rout_prefix()),
+            active=f"startswith:{model.rout_prefix()}",
+        )
 
 
-def nav_link(model: RoutableModel) -> c.Link:
-    """
-    Create a navbar link
-
-    :param model: a RoutableModel
-    :returns: Link with text=model.__name__.title() and on_click=GoToEvent(url=model.rout_prefix())
-    """
-    link = c.Link(
-        components=[c.Text(text=model.__name__.title())],
-        on_click=GoToEvent(url=model.rout_prefix()),
-        active=f"startswith:{model.rout_prefix()}",
-    )
-    return link
-
-
-def ui_link(title: str, url: str, on_click=None, class_name="") -> c.Link:
-    """
-    Create a link
-
-    :param title: link title
-    :param url: link url
-    :param on_click: on_click event
-    :param class_name: css class names as a string
-    :returns: Link with text=title and on_click=GoToEvent(url=url)
-    """
-    if not url and not on_click:
-        logger.error("No url or on_click")
-        return c.Link(components=[c.Text(text="---")])
-    on_click = on_click or GoToEvent(url=url)
-    link = c.Link(components=[c.Text(text=title)], on_click=on_click, class_name=class_name)
-    return link
+class NavbarPR(c.Navbar):
+    @classmethod
+    def from_routable(cls, *models: RoutableModel) -> c.Navbar:
+        return cls(
+            links=[LinkPR.model_nav(_) for _ in models],
+        )
 
 
 class RoutableModel(Protocol):
