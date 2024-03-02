9d99cea2fc627a408831d4ada73289cd4971f85b
paw <pawrequest@users.noreply.github.com>
2024-03-02 13:22:51 +0000

    smash framework mostly working

--- src/pawsupport/sqlmodel_ps/__init__.py
+++ src/pawsupport/sqlmodel_ps/__init__.py
@@ -1,4 +1,4 @@
-from .sqlm import (
+from .sqlpr import (
     all_matches,
     assign_all,
     assign_rel,
@@ -8,7 +8,9 @@ from .sqlm import (
     model_map_,
     obj_in_session,
 )
+from . import sqlpr
 from .sqlm_test import engine_fxt, session_fxt
 
 __all__ = ['assign_rel', 'assign_all', 'obj_in_session', 'db_obj_matches', 'all_matches',
-           'model_map_', 'get_table_names', 'get_other_table_names', 'session_fxt', 'engine_fxt']
+           'model_map_', 'get_table_names', 'get_other_table_names', 'session_fxt', 'engine_fxt',
+           'sqlpr']
