a2ab0faf4987811fd4b448a3e5dc45dfe30d5cb7
paw <pawrequest@users.noreply.github.com>
2024-03-25 23:03:13 +0000

    smash

--- src/suppawt/get_set.py
+++ src/suppawt/get_set.py
@@ -52,7 +52,6 @@ def get_hash(obj: object) -> str:
 
     if not mapped:
         raise ValueError(f"Can't find any hashable attributes on {obj}")
-    print('HASHED:\n', mapped)
     return hash_simple_md5(mapped)
 
 
