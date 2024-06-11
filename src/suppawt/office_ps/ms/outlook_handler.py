f6dcd1722ecc1865e6ed90f8876c14044ac39c79
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-06-11 02:01:20 +0100

    only multiple

--- src/suppawt/office_ps/ms/outlook_handler.py
+++ src/suppawt/office_ps/ms/outlook_handler.py
@@ -6,43 +6,32 @@ from suppawt.office_ps.email_handler import (
     Email,
     EmailError,
     EmailHandler,
-    EmailHandlerMultipleAttachments,
-    EmailMultipleAttachments,
 )
 
-# class OutlookHandler(EmailHandler):
-#
-#     """
-#     Email handler for Outlook
-#     """
-#
-#     def send_email(self, email: Email):
-#         """
-#         Send email via Outlook
-#
-#         :param email: Email object
-#         :return: None
-#         """
-#         try:
-#             pythoncom.CoInitialize()
-#
-#             outlook = Dispatch('outlook.application')
-#             mail = outlook.CreateItem(0)
-#             mail.To = email.to_address
-#             mail.Subject = email.subject
-#             mail.Body = email.body
-#             if email.attachment_path:
-#                 mail.Attachments.Add(str(email.attachment_path))
-#                 print('Added attachment')
-#             # print('Sending email [disabled - uncomment to enable]')
-#             mail.Display()
-#             # mail = None
-#             # mail.Send()
-#         except Exception as e:
-#             logger.exception(f'Failed to send email with error: {e}')
-#             raise EmailError(f'{e.args[0]}')
-#         finally:
-#             pythoncom.CoUninitialize()
+
+def emailer(email: Email):
+    """
+    Send email via Outlook
+
+    :param email: Email object
+    :return: None
+    """
+    try:
+        pythoncom.CoInitialize()
+        outlook = Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+        mail.To = email.to_address
+        mail.Subject = email.subject
+        mail.Body = email.body
+        for att_path in email.attachment_paths:
+            mail.Attachments.Add(str(att_path))
+            print('Added attachment')
+        mail.Display()
+    except Exception as e:
+        logger.exception(f'Failed to send email with error: {e}')
+        raise EmailError(f'{e.args[0]}')
+    finally:
+        pythoncom.CoUninitialize()
 
 
 class OutlookHandler(EmailHandler):
@@ -74,48 +63,3 @@ class OutlookHandler(EmailHandler):
             raise EmailError(f'{e.args[0]}')
         finally:
             pythoncom.CoUninitialize()
-
-# def send_email(email):
-#     try:
-#         # Initialize the COM library for the current thread
-#         pythoncom.CoInitialize()
-#
-#         outlook = Dispatch('outlook.application')
-#         mail = outlook.CreateItem(0)
-#         mail.To = email.to_address
-#         mail.Subject = email.subject
-#         mail.Body = email.body
-#         if email.attachment_path:
-#             mail.Attachments.Add(str(email.attachment_path))
-#         mail.Send()
-#
-#     except Exception as e:
-#         raise EmailError(f"Outlook not installed - {e.args[0]}")
-#     finally:
-#         # Uninitialize the COM library for the current thread
-#         pythoncom.CoUninitialize()
-
-
-def emailer(email: Email):
-    """
-    Send email via Outlook
-
-    :param email: Email object
-    :return: None
-    """
-    try:
-        pythoncom.CoInitialize()
-        outlook = Dispatch('outlook.application')
-        mail = outlook.CreateItem(0)
-        mail.To = email.to_address
-        mail.Subject = email.subject
-        mail.Body = email.body
-        for att_path in email.attachment_paths:
-            mail.Attachments.Add(str(att_path))
-            print('Added attachment')
-        mail.Display()
-    except Exception as e:
-        logger.exception(f'Failed to send email with error: {e}')
-        raise EmailError(f'{e.args[0]}')
-    finally:
-        pythoncom.CoUninitialize()
