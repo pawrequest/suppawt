8981b7af852697a52ae07d9805beb3b144516c7d
paw <pawrequest@users.noreply.github.com>
2024-10-26 17:36:22 +0100

    unused path

--- src/suppawt/misc.py
+++ src/suppawt/misc.py
@@ -1,8 +1,21 @@
 from __future__ import annotations
 
 import importlib.util
+from pathlib import Path
 
 
 def can_import(module_name):
     spec = importlib.util.find_spec(module_name)
     return spec is not None
+
+
+def unused_path(filepath: Path):
+    def numbered_filepath(number: int):
+        return filepath if not number else filepath.with_stem(f"{filepath.stem}_{number}")
+
+    incremented = 0
+    lpath = numbered_filepath(incremented)
+    while lpath.exists():
+        incremented += 1
+        lpath = numbered_filepath(incremented)
+    return lpath
